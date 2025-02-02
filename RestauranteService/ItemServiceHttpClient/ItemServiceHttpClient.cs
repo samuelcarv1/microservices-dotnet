c57d4eaa65678d4b9d9f97edffa5d19c3d553080
Samuel Carvalho <samuelcarvalho2000@outlook.com>
2025-02-02 12:44:04 -0300

    Adicionar arquivos de projeto.

---
+++ RestauranteService/ItemServiceHttpClient/ItemServiceHttpClient.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using System.Text.Json;
+using RestauranteService.Dtos;
+
+namespace RestauranteService.ItemServiceHttpClient
+{
+    public class ItemServiceHttpClient : IItemServiceHttpClient
+    {
+        private readonly HttpClient _client;
+
+        public ItemServiceHttpClient(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public void EnviaRestauranteParaItemService(RestauranteReadDto readDto)
+        {
+            var conteudoHttp = new StringContent
+                (
+                    JsonSerializer.Serialize(readDto),
+                    Encoding.UTF8,
+                    "application/json"
+                );
+        }
+    }
+}
